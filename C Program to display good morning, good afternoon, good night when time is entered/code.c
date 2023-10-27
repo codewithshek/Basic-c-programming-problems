e7894a9219f3ce3131917f0a8bea11ad50069394
Data._.pirates <96473842+data-pirates07@users.noreply.github.com>
2023-10-27 10:27:09 +0530

    Update code.c

--- C Program to display good morning, good afternoon, good night when time is entered/code.c
+++ C Program to display good morning, good afternoon, good night when time is entered/code.c
@@ -11,15 +11,20 @@ void main() {
 
     if (hours >= 0 && hours < 5) {
         printf("Good Night\n");
-    } else if (hours >= 5 && hours < 12) {
+    } 
+    else if (hours >= 5 && hours < 12) {
         printf("Good Morning\n");
-    } else if (hours >= 12 && hours < 16) {
+    } 
+    else if (hours >= 12 && hours < 16) {
         printf("Good After Noon\n");
-    } else if (hours >= 16 && hours < 20) {
+    } 
+    else if (hours >= 16 && hours < 20) {
         printf("Good Evening\n");      
-    } else if (hours >= 20 && hours <= 23) {
+    } 
+    else if (hours >= 20 && hours <= 23) {
         printf("Good Night\n");
-    } else {
+    } 
+    else {
         printf("Invalid time\n");
     }
 }
