dcd87c62e18b199405565b36293f36afe50e28c6
D ABHISHEK YADAV <96473842+codewithshek@users.noreply.github.com>
2023-10-29 16:08:01 +0530

    Create code1.c

---
+++ C Program to read three numbers and arrange in ascending order/code/code1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, temp;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    if(a > c) {
+        temp = a;
+        a = c;
+        c = temp;
+    }
+
+    if(b > c) {
+        temp = b;
+        b = c;
+        c = temp;
+    }
+
+    printf("The numbers in ascending order are: %d, %d, %d", a, b, c);
+
+    return 0;
+}
